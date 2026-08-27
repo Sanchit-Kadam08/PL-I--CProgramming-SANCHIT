e67b89712489ed5a59ce52e523af4becc6209416
Sanchit-Kadam08 <94956848+Sanchit-Kadam08@users.noreply.github.com>
2026-08-27 20:52:46 +0530

    Add files via upload

---
+++ SAK3.c
@@ -0,0 +1,15 @@
+//Program(3)- Write a program to calculate the area of a circle.
+
+//Pre-Defined Value
+
+# include <stdio.h>
+int main()
+{
+    float radius = 1.1;
+    float area;
+
+    area = (3.14*radius*radius);
+    printf("\n Area of Circle = %f ", area);
+
+    return 0;
+}
